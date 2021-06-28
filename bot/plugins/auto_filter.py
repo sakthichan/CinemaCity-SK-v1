f96001bcd4c539b6c21fb20ba89de4e246a2358d
sakthichan <85820819+sakthichan@users.noreply.github.com>
2021-06-28 23:01:28 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"⚡ Powered By @CC_links_SK.. \n Found {(len_results)} Results For Your Request: <code>{query}</code>",
+                text=f"⚡ Powered By @CC_links_SK.. \n\n Found {(len_results)} Results For Your Request: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
