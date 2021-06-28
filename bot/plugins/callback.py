cacd2327c89fcfe44a0c45427782f36eb2f95c7f
sakthichan <85820819+sakthichan@users.noreply.github.com>
2021-06-28 22:44:05 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>⚡ Powered By @CC_links_SK ⚡.. \n Found</i> <code>{leng}</code> <i>Results For Your Request:</i> <code>{query}</code>"
+    text=f"<i> \n ⚡ Powered By @CC_links_SK ⚡.. \n Found</i> <code>{leng}</code> <i>Results For Your Request:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
