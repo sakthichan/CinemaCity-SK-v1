89b533c839c7762d2fe86d00783b7712994167a1
sakthichan <85820819+sakthichan@users.noreply.github.com>
2021-07-12 14:41:12 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -81,7 +81,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if ((index_val + 1 )== max_pages) or ((index_val + 1) == len(results)): # Max Pages
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})")
+            InlineKeyboardButton("⏪ 𝐁𝐚𝐜𝐤", callback_data=f"navigate({index_val}|back|{query})")
         ])
 
     elif int(index_val) == 0:
@@ -89,13 +89,13 @@ async def cb_navg(bot, update: CallbackQuery):
 
     else:
         temp_results.append([
-            InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
-            InlineKeyboardButton("Next ⏩", callback_data=f"navigate({index_val}|next|{query})")
+            InlineKeyboardButton("⏪ 𝐁𝐚𝐜𝐤", callback_data=f"navigate({index_val}|back|{query})"),
+            InlineKeyboardButton("𝐍𝐞𝐱𝐭 ⏩", callback_data=f"navigate({index_val}|next|{query})")
         ])
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"📀 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 📀", callback_data="ignore")
+            InlineKeyboardButton(f"📀 𝐏𝐚𝐠𝐞 {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 📀", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>⚡ Powered By @CC_links_SK ⚡.. \n\n Found</i> <code>{leng}</code> <i>Results For Your Request:</i> <code>{query}</code>"
+    text=f"<i>⚡ 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @CC_links_SK ⚡.. \n\n 𝐅𝐨𝐮𝐧𝐝</i> <code>{leng}</code> <i>𝐑𝐞𝐬𝐮𝐥𝐭𝐬 𝐅𝐨𝐫 𝐘𝐨𝐮𝐫 𝐑𝐞𝐪𝐮𝐞𝐬𝐭:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
